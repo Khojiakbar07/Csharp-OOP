53166cafe717698975d8017c415e8a2994ab3fb2
Hojiakbar Zokirov <108798105+Khojiakbar07@users.noreply.github.com>
2023-01-01 13:41:43 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Student[] s = new Student[5];// Array of objects.
+            s[0] = new Student( "Sherlock Holmes",  "B-221 Street", 48, "sherlocked@gmail.com");
+            s[1] = new Student( "Will Smitt", "New York City", 50, "willsmitt@gmail.us");
+            s[2] = new Student( "Jason Statham", "Washington", 55 , "jasonstatham@icloud.us");
+            s[3] = new Student( "Michael Jakcson", "Brooklyn", 52, "michaelj@icloud.com");
+            s[4] = new Student( "Tom Holland", "Queens",  26, "tomhollandd@gmail.com");
+            s[5] = new Student( "Ryan Ryolds", "New York", 33, "ryanryolds@gmail.us");
+            Console.ReadLine();*/
+
+            Console.Write("Enter your Name: ");
+            string name = Convert.ToString(Console.ReadLine());
+            Console.Write("Enter your Address: ");
+            string address = Convert.ToString(Console.ReadLine());
+            Console.Write("Enter your Age: ");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter your Email: ");
+            string Email = Convert.ToString(Console.ReadLine());
+
+            Student student = new Student(name, address,age, Email);
+            Console.WriteLine(student);
+
+            Console.ReadLine();
+
+        }
+    }
+}
+          
---
+++ Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Student
+    {
+        private string Name;
+        private string Email;
+        private string Address;
+        private int Age;
+
+
+        public Student(string aName, string aAddress, int aAge,string aEmail)
+        {
+            this.Name= aName;
+            this.Address = aAddress;
+            this.Age = aAge;
+            this.Email = aEmail;
+            
+            
+        }
+        /*getter and setter*/
+        public string name 
+        {
+            get { return name; }
+            set {
+                    Name = value;
+                }
+
+        }
+        public string email
+        {
+            get { return email; }
+            set
+            {
+                Email = value;
+            }
+
+        }
+        public string address
+        {
+            get { return address; }
+            set
+            {
+                Address = value;
+            }
+
+        }
+        public int age
+        {
+            get { return age; }
+            set
+            {
+                Age = value;
+            }
+
+        }
+        /*getter and setter end*/
+        public String toString()
+        {
+            return "Name: " + name + ", Address: " + address + ", Age: " + age + ", Email: " + email;
+        }
+
+        
+    }
+
+}
+
+
