b7b3436ff252a0bba42c91cb74f98cf0e8154e67
Hojiakbar Zokirov <108798105+Khojiakbar07@users.noreply.github.com>
2023-01-05 23:02:38 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using OOP;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,36 +9,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
+
 namespace OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*Student[] s = new Student[5];// Array of objects.
-            s[0] = new Student( "Sherlock Holmes",  "B-221 Street", 48, "sherlocked@gmail.com");
-            s[1] = new Student( "Will Smitt", "New York City", 50, "willsmitt@gmail.us");
-            s[2] = new Student( "Jason Statham", "Washington", 55 , "jasonstatham@icloud.us");
-            s[3] = new Student( "Michael Jakcson", "Brooklyn", 52, "michaelj@icloud.com");
-            s[4] = new Student( "Tom Holland", "Queens",  26, "tomhollandd@gmail.com");
-            s[5] = new Student( "Ryan Ryolds", "New York", 33, "ryanryolds@gmail.us");
-            Console.ReadLine();*/
+           // List<string> arraylist = new List<string>();
+            ArrayList arraylist = new ArrayList();
+            Student[] s = new Student[5];
+            Student student1 = new Student( "Sherlock Holmes",  "B-221 Street", 48, "sherlocked@gmail.com");
+            Student student2 = new Student("Will Smitt", "New York City", 50, "willsmitt@gmail.us");
+            Student student3 = new Student("Jason Statham", "Washington", 55, "jasonstatham@icloud.us");
+            Student student4 = new Student("Michael Jakcson", "Brooklyn", 52, "michaelj@icloud.com");
+            Student student5 = new Student("Tom Holland", "Queens", 26, "tomhollandd@gmail.com");
+            Student student6 = new Student("Ryan Ryolds", "New York", 33, "ryanryolds@gmail.us");
+            arraylist.Add(student1);
+            arraylist.Add(student2);
+            arraylist.Add(student3);
+            arraylist.Add(student4);
+            arraylist.Add(student5);
+            arraylist.Add(student6);
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter your Name: ");
+                string name = Convert.ToString(Console.ReadLine());
+                Console.Write("Enter your Address: ");
+                string address = Convert.ToString(Console.ReadLine());
+                Console.Write("Enter your Age: ");
+                int age = Convert.ToInt16(Console.ReadLine());
+                Console.Write("Enter your Email: ");
+                string Email = Convert.ToString(Console.ReadLine());
 
-            Console.Write("Enter your Name: ");
-            string name = Convert.ToString(Console.ReadLine());
-            Console.Write("Enter your Address: ");
-            string address = Convert.ToString(Console.ReadLine());
-            Console.Write("Enter your Age: ");
-            int age = Convert.ToInt16(Console.ReadLine());
-            Console.Write("Enter your Email: ");
-            string Email = Convert.ToString(Console.ReadLine());
-
-            Student student = new Student(name, address,age, Email);
-            Console.WriteLine(student);
-
+            }
+            Console.WriteLine(arraylist.ToString());
             Console.ReadLine();
-
+            Console.WriteLine("Please enter an address: ");
+            String anyAddress = Convert.ToString(Console.ReadLine());
+            //address
+            for (int i = 0; i < s.Length; i++) {
+               if (s[i].GetAddress().equals(anyAddress));
+                    Console.WriteLine(s[i]);
+                }
+            //---------
+            for (int i=0; i<s.Length; i++)
+            {
+                if (arraylist.getEmail().contains("gmail.com"))
+                    Console.WriteLine(arraylist);
+            }
         }
     }
 }
-          
+
+
+        
